9493bc196a11b34c6b8fc187963ff930e7d102f5
Jitesh4565 <jitujadhav28@gmail.com>
2025-05-13 20:29:07 +0530

    port changes

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import connectDB from './config/db';
 import app from './app';
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT||5000;
 
 connectDB().then(()=>{
     app.listen(PORT,()=>console.log(`Server Running on Port ${PORT}`));
